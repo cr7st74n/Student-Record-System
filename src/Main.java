74e8002f3e375e84613e9190d93368ddd775c908
cr7st74n <cris-082011@hotmail.com>
2023-12-06 14:41:33 -0500

    first part od the SRS project for MSU

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Welcome to the Student Record System for Montclair State University");
+        System.out.println("Please select an option to continue");
+    }
+}
---
+++ src/People.java
@@ -0,0 +1,16 @@
+public class People {
+    private int age;
+    private String gender;
+    private Address address;
+
+    public People(int age, String gender, Address address) {
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+    }
+
+    // Implement methods for setting and retrieving people-specific information (age, gender, and address).
+
+    // Additional methods or getters/setters can be added as needed.
+}
+
